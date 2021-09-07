4e03769ee6cbb93a931473bad48e6d9351edbc18
Thanaphon06 <68720136+Thanaphon06@users.noreply.github.com>
2021-09-07 20:59:45 +0700

    Click to move
    dont need rigitbody 2D just box colider

---
+++ ClickMove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMove : MonoBehaviour
+{
+    private Vector2 target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0))
+        {
+            target = new Vector2(mousPos.x, transform.position.y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+    }
+}
